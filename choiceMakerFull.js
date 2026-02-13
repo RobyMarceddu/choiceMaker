4247d90756d21b2185a00ab06a6ba6648b1ca14b
Roberta Marceddu <159711531+RobyMarceddu@users.noreply.github.com>
2026-02-13 12:08:59 +0100

    Refactor mode handling and output in choiceMakerFull.js
    Refactor choiceMakerFull.js to improve mode handling and output messages.

--- choiceMakerFull.js
+++ choiceMakerFull.js
@@ -2,43 +2,45 @@ const prompt = require('prompt-sync')();
 
 const modeChoice = Number(prompt("Choose a mode: "))
 
-const wordsMode = () => {modeChoice === 1};
-const numberMode = () => {modeChoice === 2};
+const wordsMode = () => {if (modeChoice === 1) {
+
+    const words = ['yes','no', 'maybe', 'ask me later'];
+
+const randomIndex = Math.floor(Math.random() * words.length);
+
+console.log('Magic Ball says: ' + [randomIndex]);
+
+return words[randomIndex];
+}};
+
+
+const numberMode = () => {if (modeChoice === 2) {
+    
+    let min = Number(prompt("minimum number: "));
+    let max = Number(prompt("maximum number: "));
+
+    const numbers = Math.floor(Math.random() * (max - min + 1)) + min;
+
+    console.log("Lucky number is: " + numbers);;
+    return numbers;
+}};
 
 
 const mode = modeChoice === 1 ? wordsMode : numberMode;
 
 console.log(mode);
 
-
-function choiceMakerWorlds () {
-    const words = ['yes','no', 'maybe', 'ask me later'];
-
-const randomIndex = Math.floor(Math.random() * words.length);
-
-return words[randomIndex];
-}
-
-function choiceMakerNumber (min, max) {
-    
-    const numbers = Math.floor(Math.random() * (max - min + 1)) + min;
-    return numbers;
-}
-
-
-
-if (modeChoice === 1) {
-    const words = choiceMakerWorlds();
-    console.log(words);
-}
- 
+if (modeChoice === 1 ) {
+    wordsMode();
+} 
 else if (modeChoice === 2) {
-    let minRange = Number(prompt("minimum number: "));
-    let maxRange = Number(prompt("maximum number: "));
-
-
-    console.log(choiceMakerNumber(minRange, maxRange));
+    numberMode();
+}
+else {
+    console.log("Invalid mode choice");
+    return;
     }
+
 
 
 
